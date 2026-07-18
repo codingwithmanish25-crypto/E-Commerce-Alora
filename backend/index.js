1fdca96f1c9f657df5e54a569765d22fb4aa56fe
Manish <codingwithmanish25@gmail.com>
2026-07-18 12:28:15 +0530

    lcoal deloyments change to url

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ const app = express()
 
 // Middleware setup
 app.use(cors({
-  origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'http://localhost:5000', 'http://127.0.0.1:5000'],
+  origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'http://localhost:5000', 'http://127.0.0.1:5000','https://e-commerce-alora.vercel.app/'],
   credentials: true // <-- ZAROORI: Taaki cookies incoming/outgoing access ho sakein
 }));
 
